858edb2480a54af9387eb3bb77af104c2c8f0a60
martincz <martincz@mokeedev.com>
2016-11-19 22:39:28 +0800

    Revert "Calendar: Optimize the UI of Chinese calendar text"
    This reverts commit 777664a2da2658a06c5db217936c28387ad84520.
    
    Change-Id: If6434b7d2fca250fafb2e17c02bd00fa7cde4eb2

--- src/com/android/calendar/month/MonthWeekEventsView.java
+++ src/com/android/calendar/month/MonthWeekEventsView.java
@@ -16,7 +16,6 @@
 
 package com.android.calendar.month;
 
-import com.android.calendar.CalendarViewAdapter;
 import com.android.calendar.Event;
 import com.android.calendar.LunarUtils;
 import com.android.calendar.R;
@@ -730,7 +729,6 @@ public class MonthWeekEventsView extends SimpleWeekView {
                 // adjust the year and month
                 int year = time.year;
                 int month = time.month;
-                int defalutColor = mMonthNumPaint.getColor();
                 int julianMondayDay = time.monthDay;
                 int monthDay = Integer.parseInt(mDayNumbers[i]);
                 if (monthDay != julianMondayDay) {
@@ -754,7 +752,6 @@ public class MonthWeekEventsView extends SimpleWeekView {
                 LunarUtils.get(getContext(), year, month, monthDay,
                         LunarUtils.FORMAT_LUNAR_SHORT | LunarUtils.FORMAT_MULTI_FESTIVAL, false,
                         infos);
-                boolean isChineseFestival = LunarUtils.isChineseFestival(year, month, monthDay);
                 if (infos.size() > 0) {
                     float originalTextSize = mMonthNumPaint.getTextSize();
                     mMonthNumPaint.setTextSize(TEXT_SIZE_LUNAR);
@@ -775,14 +772,8 @@ public class MonthWeekEventsView extends SimpleWeekView {
                             infoX = x;
                             infoY = y + (mMonthNumHeight + LUNAR_PADDING_LUNAR) * (num + 1);
                         }
-
-                        if (isChineseFestival) {
-                            mMonthNumPaint.setColor(getContext().getResources()
-                                    .getColor(R.color.green));
-                        }
                         canvas.drawText(info, infoX, infoY, mMonthNumPaint);
                         ++num;
-                        mMonthNumPaint.setColor(defalutColor);
                     }
 
                     // restore the text size.
